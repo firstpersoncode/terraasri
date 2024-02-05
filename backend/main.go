1b05d4fc4ceb2f1fe5025d8fa80274206b49377d
firstpersoncode <fpcdatadriver@gmail.com>
2024-02-05 07:32:52 +0700

    update entry point

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -11,30 +13,48 @@ import (
 // main function
 func main() {
 	// connect to database
-	// db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer db.Close()
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// create table if it doesn't exist
-	// _, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	defer db.Close()
 
-	// create router
+	initSecretMessageTable(db)
 	router := mux.NewRouter()
-	// router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
-	// router.HandleFunc("/api/go/users", createUser(db)).Methods("POST")
-	// router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("GET")
-	// router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
-	// router.HandleFunc("/api/go/users/{id}", deleteUser(db)).Methods("DELETE")
 	router.HandleFunc("/api/go/calculate-diagonal-sum/{size}", calculateDiagonalSum).Methods("GET")
 	router.HandleFunc("/api/go/find-common-slot", findCommonSlot).Methods("POST")
-
-	// wrap the router with CORS and JSON content type middlewares
+	router.HandleFunc("/api/go/is-circular-palindrome/{word}", isCircularPalindrome).Methods("GET")
+	router.HandleFunc("/api/go/self-numbers/{number}", selfNumbers).Methods("GET")
+	router.HandleFunc("/api/go/secret-message/token", addToken(db)).Methods("POST")
+	router.HandleFunc("/api/go/secret-message/encrypt", encryptMessage(db)).Methods("POST")
+	router.HandleFunc("/api/go/secret-message/decrypt", decryptMessage(db)).Methods("POST")
 	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
-	// start server
 	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Check if the request is for CORS preflight
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass down the request to the next middleware (or final handler)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set JSON Content-Type
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
